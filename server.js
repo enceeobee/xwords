774bc2a132c7443a6bb85aa65adfb6bc2ebe316f
Nate Bisbee <enceeobee@users.noreply.github.com>
2019-04-03 17:36:26 -0700

    Add Helmet

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const axios = require('axios')
 const express = require('express')
+const helmet = require('helmet')
 const cors = require('express-cors')
 
 const app = express()
 
+app.use(helmet())
 app.use(cors({ allowedOrigins: ['localhost:3000'] }))
 
 app.get('/xword', (req, res, next) => {
@@ -14,11 +16,6 @@ app.get('/xword', (req, res, next) => {
   axios('https://www.xwordinfo.com/JSON/Data.aspx', {
     params,
     headers: {
-      'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-      'accept-language': 'en-US,en;q=0.9',
-      'upgrade-insecure-requests': '1',
-      'Upgrade-Insecure-Requests': 1,
-      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36',
       Referer: 'https://www.xwordinfo.com/JSON/'
     }
   })
