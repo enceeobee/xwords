3067c6113c2c9f024b708c94a5058b92502cd5a5
Nate Bisbee <enceeobee@users.noreply.github.com>
2019-04-04 14:01:24 -0700

    Updates

---
+++ precache-manifest.06845ef9841d5e8db2c0f68e8cedc3eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d525bbfdc9e75ea223b",
+    "url": "/xwords/static/js/runtime~main.42fedd96.js"
+  },
+  {
+    "revision": "26c22954f9e122c8050d",
+    "url": "/xwords/static/js/main.c5bf55d1.chunk.js"
+  },
+  {
+    "revision": "b1ac051b82fba220e6a4",
+    "url": "/xwords/static/js/2.f3827dbd.chunk.js"
+  },
+  {
+    "revision": "26c22954f9e122c8050d",
+    "url": "/xwords/static/css/main.e431e101.chunk.css"
+  },
+  {
+    "revision": "b1ac051b82fba220e6a4",
+    "url": "/xwords/static/css/2.72baefdb.chunk.css"
+  },
+  {
+    "revision": "69369c2425122781ab5d616a1320ced2",
+    "url": "/xwords/index.html"
+  }
+];
